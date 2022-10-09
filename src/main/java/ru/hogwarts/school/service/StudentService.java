368389daf50fead9bd448f204d3870aad32db6da
Dmitriy <Dimitriy303@yandex.ru>
2022-10-09 16:09:25 +0900

    correctedAndSuccessfullyLaunched

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,13 +1,14 @@
 package ru.hogwarts.school.service;
 
+
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.StudentRepository;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 
+import java.util.Collection;
+
+@Service
 
 public class StudentService {
    private final StudentRepository studentRepository;
