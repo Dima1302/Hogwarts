23f95fb350066ebef6f79bc43f025e6e2b65b528
Dmitriy <Dimitriy303@yandex.ru>
2022-10-18 23:39:47 +0900

    task 3.6

--- src/test/java/ru/hogwarts/school/HogwartsApplicationTests.java
+++ src/test/java/ru/hogwarts/school/HogwartsApplicationTests.java
@@ -1,13 +1,21 @@
 package ru.hogwarts.school;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import ru.hogwarts.school.controller.StudentController;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HogwartsApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
+
+
+
 
 }
